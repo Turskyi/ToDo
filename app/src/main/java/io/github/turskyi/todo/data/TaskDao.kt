129fd790085d7b7e5a8d50c721ce4d07939475f7
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2021-03-08 15:29:45 +0200

    add filter

--- app/src/main/java/io/github/turskyi/todo/data/TaskDao.kt
+++ app/src/main/java/io/github/turskyi/todo/data/TaskDao.kt
@@ -1,16 +1,28 @@
 package io.github.turskyi.todo.data
 
 import androidx.room.*
+import io.github.turskyi.todo.data.TaskEntity.Companion.COLUMN_COMPLETED
+import io.github.turskyi.todo.data.TaskEntity.Companion.COLUMN_CREATED
 import io.github.turskyi.todo.data.TaskEntity.Companion.COLUMN_IMPORTANT
 import io.github.turskyi.todo.data.TaskEntity.Companion.COLUMN_NAME
 import io.github.turskyi.todo.data.TaskEntity.Companion.TABLE_TASKS
+import io.github.turskyi.todo.ui.tasks.SortOrder
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
 
-    @Query("SELECT * FROM $TABLE_TASKS WHERE $COLUMN_NAME LIKE '%' || :searchQuery || '%' ORDER BY $COLUMN_IMPORTANT DESC")
-    fun getTasks(searchQuery: String): Flow<List<TaskEntity>>
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<TaskEntity>> =
+        when(sortOrder) {
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query, hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM $TABLE_TASKS WHERE ($COLUMN_COMPLETED != :hideCompleted OR $COLUMN_COMPLETED = 0) AND $COLUMN_NAME LIKE '%' || :searchQuery || '%' ORDER BY $COLUMN_IMPORTANT DESC, $COLUMN_NAME")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<TaskEntity>>
+
+    @Query("SELECT * FROM $TABLE_TASKS WHERE ($COLUMN_COMPLETED != :hideCompleted OR $COLUMN_COMPLETED = 0) AND $COLUMN_NAME LIKE '%' || :searchQuery || '%' ORDER BY $COLUMN_IMPORTANT DESC, $COLUMN_CREATED")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<TaskEntity>>
 
     /* "suspend" added to function for using in the background thread */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
--- app/src/main/java/io/github/turskyi/todo/data/TaskEntity.kt
+++ app/src/main/java/io/github/turskyi/todo/data/TaskEntity.kt
@@ -13,15 +13,18 @@ import java.text.DateFormat
 data class TaskEntity(
     @ColumnInfo(name = COLUMN_NAME) val name: String,
     @ColumnInfo(name = COLUMN_IMPORTANT) val important: Boolean = false,
-    val completed: Boolean = false,
-    val created: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = COLUMN_COMPLETED) val completed: Boolean = false,
+    @ColumnInfo(name = COLUMN_CREATED) val created: Long = System.currentTimeMillis(),
     @PrimaryKey(autoGenerate = true) val id: Int = 0
 ) : Parcelable {
     companion object {
         const val TABLE_TASKS = "task_table"
         const val COLUMN_NAME = "name"
         const val COLUMN_IMPORTANT = "important"
+        const val COLUMN_COMPLETED = "completed"
+        const val COLUMN_CREATED = "created"
     }
+
     val createdDateFormatted: String
         get() = DateFormat.getDateTimeInstance().format(created)
 }
